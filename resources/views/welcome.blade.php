a03d5abfaad312c7a4d5d84b469902cfc3a5f6ee
Pedro <fatsnake8@gmail.com>
2016-07-15 11:33:15 -0600

    .

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,31 +1,32 @@
+
 @extends('parent')
   @section('contenido')
   <div class="row max-container">
     <div class="col-sm-3 center-align">
       <div class="dropdown center center-align">
         <ul class="center-align no-style-list">
-          <a href="#"><li class="list-element">Pizza</li></a>
-          <a href="#"><li class="list-element">Hamburguesa</li></a>
-          <a href="#"><li class="list-element">Sushi</li></a>
-          <a href="#"><li class="list-element">Comida China</li></a>
+          <li><a href="#">Pizza</a></li>
+          <li><a href="#">Hamburguesa</a></li>
+          <li><a href="#">Sushi</a></li>
+          <li><a href="#">Comida China</a></li>
         </ul>
       </div>
     </div>
     <div class="col-sm-9">
-      <div class="row">
-          @if($empresas !=null)  
-          @foreach($empresas as $emp)
-
+      <div class="row">  
         <div class="col-sm-3 card">
-        <h3>empresa:</h3>
-        {{$emp->nombre}} <br>
-        <h3>giro:</h3>
-        {{$emp->giro}} <br>
+          <h2>Producto #1</h2>
+          Producto de prueba.
         </div>
-        @endforeach
-        @else
-        <h1>No hay empresas actualmente</h1>
-        @endif
+        <div class="col-sm-3 card">
+          Producto 2
+        </div>
+        <div class="col-sm-3 card">
+          Producto 3
+        </div>
+        <div class="col-sm-3 card">
+          Producto 4
+        </div>
       </div>
     </div>
   </div>
