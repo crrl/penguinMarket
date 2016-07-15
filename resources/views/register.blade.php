ed167067f8b4caa795555ebffe54548074addd4f
Christian Rocha <christian.rocha@roket.mx>
2016-07-15 11:19:08 -0600

    .

---
+++ resources/views/register.blade.php
@@ -0,0 +1,7 @@
+@extends('parent')
+@section('contenido')
+  <div class="button-center">
+    <a class="btn btn-info " href="companyRegister">Empresa</a>
+    <button class="btn btn-info">Usuario</button>
+  </div>
+@stop
