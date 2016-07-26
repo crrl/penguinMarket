9d15f63734b6de4b072278f4cd9cfbe12d2c97fd
Christian Rocha <christian.rocha@roket.mx>
2016-07-26 12:38:51 -0600

    creacion de vistas.

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -2,6 +2,6 @@
 @section('contenido')
   <div class="button-center">
     <a class="btn btn-info" href="companyRegister">Empresa</a>
-    <a class="btn btn-info" href="userRegister">Usuario</a>
+    <a class="btn btn-info" href="userRegister">Cliente</a>
   </div>
 @stop
